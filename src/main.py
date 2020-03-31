0af3c6a3a405a246e111d5d964b3cd29f4d7e0e1
Komron <komron_aripov@cdl.ch>
2020-04-01 00:38:46 +0200

    register new service and callback handlers

--- src/main.py
+++ src/main.py
@@ -2,21 +2,24 @@ from pathlib import Path
 import logging
 import json
 
-from handlers import admin
+from handlers import admin, service, callback
 
 from aiogram import Bot, Dispatcher, executor
 
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s - %(name)s - %(message)s'
 )
 
-config = json.load(open(Path.cwd().joinpath('config.json')))
-bot = Bot(token=config['TOKEN'])
+CONFIG = json.load(open(Path.cwd().joinpath('src/config.json')))
+bot = Bot(token=CONFIG['TOKEN'])
 dp = Dispatcher(bot=bot)
 
 dp.register_message_handler(admin.stats, commands=['stats'])
+dp.register_message_handler(service.start, commands=['start'])
+dp.register_message_handler(service.lang, commands=['lang'])
+dp.register_callback_query_handler(callback.button_lang)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
