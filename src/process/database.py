022c08e799f862100b3034dedf75091a0a367579
Komron <komron_aripov@cdl.ch>
2020-04-01 00:36:49 +0200

    user database model

--- src/process/database.py
+++ src/process/database.py
@@ -3,11 +3,18 @@ from peewee import Model, SqliteDatabase
 from peewee import CharField, IntegerField, BooleanField
 
 
+db = SqliteDatabase(Path.cwd().joinpath('src/data/users.db'))
+
+
 class User(Model):
-    user_id = IntegerField()
-    language = CharField()
-    requests = IntegerField()
-    is_ban = BooleanField()
+    user_id = IntegerField(primary_key=True)
+    language = CharField(default='none')
+    requests = IntegerField(default=0)
+    is_ban = BooleanField(default=True)
 
     class Meta:
-        database = SqliteDatabase(Path.cwd().joinpath('src/data/users.db'))
+        database = db
+
+
+db.connect()
+db.create_tables([User])
