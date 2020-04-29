5810abad89c0aa6a13438dd04bdc3d6ab8ed754e
Komron <komron_aripov@cdl.ch>
2020-04-29 19:31:40 +0200

    telethon session files setup

---
+++ scripts/telethon_setup.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import json
+from telethon import TelegramClient
+
+CONFIG = json.load(open(Path.cwd().joinpath('src/config.json')))
+
+client = TelegramClient(
+    session=str(Path.cwd().joinpath(CONFIG['TELETHON']['SESSION'])),
+    api_id=CONFIG['TELETHON']['API_ID'],
+    api_hash=CONFIG['TELETHON']['API_HASH']
+)
+
+
+async def main():
+    me = await client.get_me()
+    print(me.stringify())
+
+
+with client:
+    client.loop.run_until_complete(main())
