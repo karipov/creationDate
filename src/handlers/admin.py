6000248c47e867ddde8b5c3ea6351765854905d4
Komron <komron_aripov@cdl.ch>
2020-05-07 02:16:14 +0200

    total requests and logging added to admin commands

--- src/handlers/admin.py
+++ src/handlers/admin.py
@@ -6,6 +6,7 @@ import json
 import logging
 
 from aiogram import types
+from peewee import fn
 from process.database import User
 
 
@@ -15,27 +16,47 @@ logger = logging.getLogger(__name__)
 
 async def stats(message: types.Message):
     payload = message.get_args().split()
+
+    base_log_msg = (
+        f"Admin {message.from_user.id} requested {payload} with result"
+    )
+
     subcommands = [
-        ['total'],
-        ['file'],
-        ['lang']
+        'total',
+        'file',
+        'lang',
+        'reqs'
     ]
 
-    if payload == ['total']:
+    if 'total' in payload:
         total_users = User.select().where(
             User.is_ban == False
         ).count()  # noqa: E712
+
+        logger.info(f"{base_log_msg} {total_users}")
+
         await message.answer(total_users)
 
-    elif payload == ['file']:
+    elif 'file' in payload:
         file = open(Path.cwd().joinpath('src/data/users.db'), 'rb')
         input_file = types.InputFile(file)
+
+        logger.info(f"{base_log_msg} <file>")
 
         await message.answer_document(input_file)
 
         file.close()
 
-    elif payload == ['lang']:
+    elif 'reqs' in payload:
+        total_requests = User.select(
+            fn.SUM(User.requests)
+        ).scalar(as_tuple=True)
+
+        logger.info(f"{base_log_msg} {total_requests[0]}")
+
+        await message.answer(total_requests[0])
+
+    elif 'lang' in payload:
         text = 'Language Percentages:'
         top_list = []
 
@@ -50,10 +71,12 @@ async def stats(message: types.Message):
             except ZeroDivisionError:
                 percent = 0.0
 
-            top_list.append([lang, percent])
+            top_list.append([lang, round(percent, 4)])
 
         for lang, per in sorted(top_list, key=lambda x: x[1], reverse=True):
             text += f'\n<pre>{lang}: {per:.2%}</pre>'
+
+        logger.info(f"{base_log_msg} {top_list}")
 
         await message.answer(
             text=text,
@@ -61,4 +84,5 @@ async def stats(message: types.Message):
         )
 
     else:
+        logger.info(f"{base_log_msg} no result.")
         await message.answer(subcommands)
